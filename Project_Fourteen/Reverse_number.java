5580b512c2e5e33ba254fd48c5805320f604ab9e
ali-gh-84 <alighafari.a.84@gmail.com>
2024-10-08 08:03:19 -0700

    update new file

--- Project_Fourteen/Reverse_number.java
+++ Project_Fourteen/Reverse_number.java
@@ -19,12 +19,20 @@ public class Reverse_number {
         }
         System.out.println("count of number is : " + count);
 
-        while (count > -1) {
+        count --; 
+
+        for (int i = count ; i > -1 ; i--) { 
             int l = number % 10;
-            total += (l*Math.pow(10,count-1));
-            number = number / 10;
-            count -= 1;
+            total  = (total * 10) + l; 
+            number = number / 10; 
         }
         System.out.print("reverse number is : " + total);
     }
 }
+
+        // while (count > -1) {
+            // int l = number % 10;
+            // total += (l*Math.pow(10,count-1));
+            // number = number / 10;
+            // count -= 1;
+        // }
